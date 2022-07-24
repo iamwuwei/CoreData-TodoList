d289fdfebd72ad202f423311f821b72860212913
Wu Wei <vc3104@gmail.com>
2022-07-25 08:40:21 +0900

    Initial Commit

---
+++ CoreData-Todo/CoreData_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_TodoApp.swift
+//  CoreData-Todo
+//
+//  Created by Wei Wu on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
